900e67d511875298337913ead4b903d5918dc183
olivier.bensemhoun <olivier.bensemhoun@epita.fr>
2026-02-19 12:34:28 +0100

    build: api-middelware backend

---
+++ backend/src/main/java/com/vapeur/backwork/BackWorkApplication.java
@@ -0,0 +1,13 @@
+package com.vapeur.backwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackWorkApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/vapeur/backwork/BackWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vapeur.backwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
