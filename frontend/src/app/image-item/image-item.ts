4e36dd9608d58df8d1267fb139314ce9a95d102d
Olivier Bensemhoun <olivier.bensemhoun@epita.fr>
2026-03-26 18:31:42 +0100

    add list of game

--- frontend/src/app/image-item/image-item.ts
+++ frontend/src/app/image-item/image-item.ts
@@ -9,23 +9,16 @@ import {OverlayService} from "../overlay-service";
 @Component({
   selector: 'app-image-item',
     imports: [
-        NgOptimizedImage,
-        AsyncPipe,
     ],
   templateUrl: './image-item.html',
   styleUrl: './image-item.css',
 })
 export class ImageItem {
 
-  @Input()  public id : string = "";
-  @Input() public image: PhotoSmall = PhotoSmall.PhotoSmall2( "https://cdn.search.brave.com/serp/v3/_app/immutable/assets/brave-logo-dark.5D16vJCY.svg");
+  @Input()  public id : number = 0;
+  @Input() public image: PhotoSmall = PhotoSmall.PhotoSmall2();
 
     public overlay = inject(OverlayService);
-
-    public autheur: string = "test";
-    public title:string = ""
-
-    public date:string = "";
 
 
 
@@ -35,18 +28,15 @@ export class ImageItem {
 
   ngOnInit()
   {
-      if (this.image.image != undefined)
+      if (this.image.id != -1)
       {
-          this.api.get_photo(this.id,this.image.image);
+          this.api.get_game(this.id,this.image.image);
 
           this.image.image.subscribe(a =>
           {
               if (a != undefined)
               {
                   var d = new Date(Number(a.photo.dateuploaded) * 1000)
-                  this.date = d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear();
-                  this.autheur = a.photo.owner.username;
-                  this.title = a.photo.title._content;
                   this.photoflicker = a;
               }
 
@@ -58,7 +48,7 @@ export class ImageItem {
   public showinfo()
   {
       this.overlay.overlaybehavior.next(this.photoflicker)
-      this.overlay.show(this.image,this.date)
+      this.overlay.show(this.image,this.image.release_date)
   }
 
 
