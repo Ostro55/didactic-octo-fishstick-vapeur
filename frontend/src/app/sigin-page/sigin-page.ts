41e94caea86714d0450ef59f41507fe952609fdd
olivier.bensemhoun <olivier.bensemhoun@epita.fr>
2026-04-20 14:18:32 +0200

    feat: add search

--- frontend/src/app/sigin-page/sigin-page.ts
+++ frontend/src/app/sigin-page/sigin-page.ts
@@ -1,27 +1,28 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AuthService} from "../auth-service";
+import FlickrService from "../flickr-service";
 import {Router} from "@angular/router";
 import {NgClass} from "@angular/common";
-import FlickrService from "../flickr-service";
 
 @Component({
-  selector: 'app-login-page',
+  selector: 'app-sigin-page',
   imports: [
+    FormsModule,
     ReactiveFormsModule,
     NgClass
   ],
-  templateUrl: './login-page.html',
-  styleUrl: './login-page.css',
+  templateUrl: './sigin-page.html',
+  styleUrl: './sigin-page.css',
 })
-export class LoginPage {
+export class SiginPage {
   loginForm!: FormGroup;
   isSubmitted  =  false;
   public authService: AuthService = inject(AuthService)
   public api = inject(FlickrService)
 
   constructor(
-              private router: Router, private formBuilder: FormBuilder ) { }
+      private router: Router, private formBuilder: FormBuilder ) { }
 
   ngOnInit() {
     const x  =  this.formBuilder.group({
@@ -49,35 +50,11 @@ export class LoginPage {
   }
   get formControls() { return this.loginForm.controls; }
 
-  async seConnecter(){
-    console.log(this.loginForm.value);
-    this.isSubmitted = true;
-    if(this.loginForm.invalid){
-      return 1;
-    }
-    this.authService.seConnecter(this.loginForm.value);
-    await new Promise(f => setTimeout(f, 1000));
-
-    console.log(this.authService.userInfo);
-
-    if (this.authService.userInfo != null)
-    {
-      if (this.authService.userInfo.isAdmin)
-      {
-        this.router.navigateByUrl('/admin');
-
-      }
-      else {
-        this.router.navigateByUrl('/user-page');
-
-      }
-    }
-    this.router.navigateByUrl('/admin');
-    return 0
-  }
 
   async Singup() {
-     await this.router.navigateByUrl('/singin');
+    console.log(this.loginForm.value);
+    await this.api.Create_user(this.loginForm.value['email'],this.loginForm.value['password'],true)
+    await this.router.navigateByUrl('/');
 
   }
 }
